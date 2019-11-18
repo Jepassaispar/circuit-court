912cf3528c2be11d25fbc4a7e189d054776229c7
Nicolas BISCHOFF <nicolasbischoff@MacBook-Pro-de-Nicolas.local>
2019-11-18 16:41:28 +0100

    google maps

--- routes/index.js
+++ routes/index.js
@@ -4,14 +4,14 @@ const router = express();
 router.get("/", (req, res) => {
   res.render("index", {
     js: "app",
-    css: ["homeStyle", "index"]
+    css: ["baseStyle", "index"]
   });
 });
 
 router.get("/mainPage", (req, res) => {
   res.render("mainPage", {
     js: "app",
-    css: ["baseStyle", "index"]
+    css: ["baseStyle", "index", "mainPage"]
   });
 });
 
