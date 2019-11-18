738ff0be6d4bce4231b71af72b6cb16053b4c2ba
Jepassaispar <51320916+Jepassaispar@users.noreply.github.com>
2019-11-18 12:50:18 +0100

    server live !

---
+++ server.js
@@ -0,0 +1,47 @@
+require("dotenv").config();
+require("./config/mongodb");
+
+const express = require("express");
+const app = express();
+const hbs = require("hbs");
+const session = require("express-session");
+const mongoose = require("mongoose");
+const MongoStore = require("connect-mongo")(session);
+const path = require("path");
+
+app.set("view engine", "hbs");
+app.set("views", __dirname + "/views");
+app.use(express.static("public"));
+hbs.registerPartials(__dirname + "/views/partials");
+// app.use(
+//   express.urlencoded({
+//     extended: true
+//   })
+// );
+// app.use(express.json());
+
+// app.use(
+//   session({
+//     secret: process.env.SESSION_SECRET,
+//     cookie: {
+//       maxAge: 60000
+//     }, // in millisec
+//     store: new MongoStore({
+//       mongooseConnection: mongoose.connection,
+//       ttl: 24 * 60 * 60 // 1 day
+//     }),
+//     saveUninitialized: true,
+//     resave: true
+//   })
+// );
+
+app.locals.site_url = process.env.SITE_URL;
+
+const listener = app.listen(process.env.PORT, () => {
+  console.log(`app started at ${process.env.SITE_URL}:${process.env.PORT}`);
+});
+
+const index = require("./routes/index");
+app.use("/", index);
+
+module.exports = app;
