3860b8b5876a8e6b8b008f038833c406c4aece86
Nicolas BISCHOFF <nicolasbischoff@MacBook-Pro-de-Nicolas.local>
2019-11-19 12:18:49 +0100

    signin/signUp

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express();
+// const marche = require("../Models/marche");
 
 router.get("/", (req, res) => {
   res.render("index", {
@@ -12,6 +13,18 @@ router.get("/mainPage", (req, res) => {
   res.render("mainPage", {
     js: "app",
     css: ["baseStyle", "mainPage"]
+  });
+});
+
+router.get("/signUp", (req, res) => {
+  res.render("signUp", {
+    css: ["sign", "baseStyle"]
+  });
+});
+
+router.get("/logIn", (req, res) => {
+  res.render("logIn", {
+    css: ["sign", "baseStyle"]
   });
 });
 
