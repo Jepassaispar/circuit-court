cf19de417007ae2f16580844c7dbdee8ea48b02d
Nicolas BISCHOFF <nicolasbischoff@MacBook-Pro-de-Nicolas.local>
2019-11-19 12:55:33 +0100

    super les routes que tu as supprimées

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express();
-const amapModel = require('../Models/amap');
-const gardenModel = require('../Models/jardin');
-const marketModel = require('../Models/marche');
+const amapModel = require("../Models/amap");
+const gardenModel = require("../Models/jardin");
+const marketModel = require("../Models/marche");
 
 // const marche = require("../Models/marche");
 
@@ -13,30 +13,46 @@ router.get("/", (req, res) => {
   });
 });
 
-router.post("/")
+router.post("/");
 
 router.get("/mainPage", (req, res) => {
+  gardenModel
+    .find()
+    .then(dbRes => {
+      const gardens = dbRes;
+      amapModel
+        .find()
+        .then(dbRes2 => {
+          const amaps = dbRes2;
+          marketModel
+            .find()
+            .then(dbRes3 => {
+              const markets = dbRes3;
+              res.render("mainPage", {
+                js: "app",
+                css: ["baseStyle", "mainPage"],
+                gardens: gardens,
+                amaps: amaps,
+                markets: markets
+              });
+            })
+            .catch(err => console.log(err));
+        })
+        .catch(err => console.log(err));
+    })
+    .catch(err => console.log(err));
+});
 
+router.get("/logIn", (req, res) => {
+  res.render("logIn", {
+    css: ["baseStyle", "sign"]
+  });
+});
 
-  gardenModel.find().then(dbRes => {
-    const gardens = dbRes;
-    amapModel.find().then(dbRes2 => {
-      const amaps = dbRes2;
-      marketModel.find().then(dbRes3 => {
-        const markets = dbRes3;
-        res.render("mainPage", {
-          js: "app",
-          css: ["baseStyle", "mainPage"],
-          gardens: gardens,
-          amaps: amaps,
-          markets: markets
-        });
-      }).catch(err => console.log(err))
-    }).catch(err => console.log(err))
-  }).catch(err => console.log(err))
-
-
-
+router.get("/signUp", (req, res) => {
+  res.render("signUp", {
+    css: ["baseStyle", "sign"]
+  });
 });
 
 module.exports = router;
